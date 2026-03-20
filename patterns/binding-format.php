714e16d8c40426ac5168ea795fdca05c3012d10f
Elliott Richmond <elliott@squareonemd.co.uk>
2026-03-20 10:51:05 +0000

    first commit

---
+++ patterns/binding-format.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Post format name
+ * Slug: anatomy/binding-format
+ * Categories: anatomy_post-format
+ * Description: Prints the name of the post format with the help of the Block Bindings API.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Anatomy 1.0
+ */
+
+?>
+<!-- wp:paragraph {"metadata":{"bindings":{"content":{"source":"anatomy/format"}}},"fontSize":"small"} -->
+<p class="has-small-font-size"></p>
+<!-- /wp:paragraph -->
---
+++ patterns/page-business-home.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Business homepage
+ * Slug: anatomy/page-business-home
+ * Categories: anatomy_page, featured
+ * Keywords: starter
+ * Block Types: core/post-content
+ * Post Types: page, wp_template
+ * Viewport width: 1400
+ * Description: A business homepage pattern.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twenty_Five
+ * @since Anatomy 1.0
+ */
+
+?>
+
+<!-- wp:pattern {"slug":"anatomy/cta-centered-heading"} /-->
+<!-- wp:pattern {"slug":"anatomy/overlapped-images"} /-->
+<!-- wp:pattern {"slug":"anatomy/services-3-col"} /-->
+<!-- wp:pattern {"slug":"anatomy/testimonials-large"} /-->
+<!-- wp:pattern {"slug":"anatomy/pricing-2-col"} /-->
+<!-- wp:pattern {"slug":"anatomy/cta-newsletter"} /-->
